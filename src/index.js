fd67c19a7e6b8c0fabab104187090ccb331a6bf6
Paul <test@mail.ru>
2018-04-07 20:48:25 +0400

    View articles

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import ArticleList from './ArticleList'
+import {articles} from './fixtures'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<ArticleList articles = {articles} />, document.getElementById('root'));
+registerServiceWorker();
