008210ec17efe90f7bd9855ce0dbc3694f07c880
Paul <test@mail.ru>
2018-04-07 22:41:10 +0400

    View comments

--- src/Article.js
+++ src/Article.js
@@ -1,33 +1,60 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 export default class Article extends Component {
     constructor(props){
         super(props);
+        // state - то, что можно изменять
         this.state = {
-            isOpen: false
+            isOpen: false,
+            isCommentOpen: false
         }
     }
     render() {
         const {article} = this.props;
         const {isOpen} = this.state;
+        const {isCommentOpen} = this.state;
         return (
             <div>
                 <h3>{article.title}</h3>
-                <button onClick={this.toggleOpen}>{isOpen ? 'Закрыть' : 'Открыть'}</button>
-                {this.getBody()}
+                <button className="btn btn-primary" onClick={this.toggleOpen}>{isOpen ? 'Закрыть' : 'Открыть'}</button>
+                {this.getBody()}<br/>
+                {isOpen ? <button className="btn btn-secondary" onClick={this.commentToggle}>{isCommentOpen ? 'Скрыть комментарий' : 'Показать комментарий'}</button> : null}
+                {this.getComments()}
             </div>
         );
     }
-
+    // при использовании данного синтаксиса this можно не бандить
     getBody = () => {
         if(!this.state.isOpen) return null;
         const {article} = this.props;
         return <section>{article.text}</section>
-    }
+    };
+
+    getComments = () => {
+        if(!this.state.isCommentOpen) return null;
+        // console.log(this.props);
+        const {article} = this.props;
+        return (
+            <section>Комментарии:
+            <br/>{article.comments.map((comment) => <blockquote key = {comment.id}>{comment.text}<footer>{comment.user}</footer></blockquote>)}
+            </section>
+        );
+    };
 
     toggleOpen = () => {
+        // так делать нельзя!!! Только через setState!
+        // this.state.isOpen = true;
+        // меняем state. setState - команда перестроения вирт. DOM. Эта операциа асинхронная
         this.setState({
             isOpen: !this.state.isOpen
+        });
+    };
+
+    commentToggle = () => {
+        this.setState({
+            isCommentOpen: !this.state.isCommentOpen
         });
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import ArticleList from './ArticleList'
-import {articles} from './fixtures'
+import ArticleList from './ArticleList';
+import {articles} from './fixtures';
 import registerServiceWorker from './registerServiceWorker';
 
+// в {} передаем javascript код
 ReactDOM.render(<ArticleList articles = {articles} />, document.getElementById('root'));
 registerServiceWorker();
